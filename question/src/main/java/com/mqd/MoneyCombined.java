415e240c8d272cf16254cef86fe7ab5abe00298f
SaigyoujiNono <1029259127@qq.com>
2022-01-24 19:58:19 +0800

    可重复数字组合: 动态规划、严格表、规律分析

--- question/src/main/java/com/mqd/MoneyCombined.java
+++ question/src/main/java/com/mqd/MoneyCombined.java
@@ -9,15 +9,18 @@ package com.mqd;
 public class MoneyCombined {
 
     public static void main(String[] args) {
-        int target = 10, index = 0;
+        int target = 8000, index = 0;
         int[] money = {1, 2, 3};
         long t1 = System.currentTimeMillis();
         System.out.println(solution(money, index, target));
         long t2 = System.currentTimeMillis();
         System.out.println(solution2(money, target));
         long t3 = System.currentTimeMillis();
+        System.out.println(solution3(money, target));
+        long t4 = System.currentTimeMillis();
         System.out.println("方法1: " + (t2 - t1) + "ms");
         System.out.println("方法2: " + (t3 - t2) + "ms");
+        System.out.println("方法3: " + (t4 - t3) + "ms");
     }
 
 
@@ -48,6 +51,25 @@ public class MoneyCombined {
                     ways += dp[i + 1][t - money[i] * c];
                 }
                 dp[i][t] = ways;
+            }
+        }
+
+        return dp[0][target];
+    }
+
+    public static long solution3(int[] money, int target) {
+        if (money == null || money.length == 0) {
+            return 0;
+        }
+
+        int N = money.length;
+        int[][] dp = new int[N + 1][target + 1];
+        dp[N][0] = 1;
+
+        for (int i = N - 1; i >= 0; i--) {
+            for (int t = 0; t <= target; t++) {
+                dp[i][t] = dp[i + 1][t] +
+                        ((t - money[i] < 0) ? 0 : dp[i][t - money[i]]);
             }
         }
 
