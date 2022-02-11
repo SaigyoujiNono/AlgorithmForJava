834bf7cd6d4b35c9a94dcbe5c5940e4cb3608f05
SaigyoujiNono <1029259127@qq.com>
2022-02-12 02:11:38 +0800

    用最少的6或8相加求某个数(优化solution)

--- question/src/main/java/com/mqd/BuyApple.java
+++ question/src/main/java/com/mqd/BuyApple.java
@@ -1,5 +1,6 @@
 package com.mqd;
 
+
 /**
  * 去商店买苹果，一次只能买6个装或者8个装
  * 现在想买n个苹果，求最少包装袋数量，如果不能正好买n个苹果则返回-1
@@ -8,7 +9,7 @@ public class BuyApple {
 
 
     public static void main(String[] args) {
-        int n = 224;
+        int n = 1111;
         long t1 = System.currentTimeMillis();
         System.out.println(solution(n));
         long t2 = System.currentTimeMillis();
@@ -16,27 +17,30 @@ public class BuyApple {
         long t3 = System.currentTimeMillis();
         System.out.println(solution3(n));
         long t4 = System.currentTimeMillis();
+        System.out.println(solution4(n));
+        long t5 = System.currentTimeMillis();
         System.out.println("方法1: " + (t2 - t1) + "ms");
         System.out.println("方法2: " + (t3 - t2) + "ms");
         System.out.println("方法3: " + (t4 - t3) + "ms");
+        System.out.println("方法4: " + (t5 - t4) + "ms");
     }
 
     public static int solution(int n) {
-        if (n%2!=0) return -1;
+        if (n % 2 != 0) return -1;
         if (n == 0) {
             return 0;
         }
         if (n < 0) {
             return -1;
         }
-        return min(solution(n - 6),solution(n - 8));
+        return min(solution(n - 6), solution(n - 8));
     }
 
     public static int solution2(int n) {
-        if (n%2!=0) return -1;
+        if (n % 2 != 0) return -1;
         int eight = n / 8;
         int six = 0;
-        while (eight > 0) {
+        while (eight >= 0) {
             while ((n - (eight * 8 + six * 6)) > 5) {
                 six++;
             }
@@ -48,18 +52,27 @@ public class BuyApple {
         return -1;
     }
 
-    public static int solution3(int n){
-        if (n%2!=0) return -1;
-        if (n % 8 == 0){
-            return n/8;
+    public static int solution3(int n) {
+        if (n % 2 != 0) return -1;
+        if (n % 8 == 0) {
+            return n / 8;
         }
-        if (n < 0){
+        if (n < 0) {
             return -1;
         }
-        return min(solution3(n - 6),solution3(n - 8));
+        return min(solution3(n - 6), solution3(n - 8));
     }
 
-    private static int min(int x1, int x2){
+    public static int solution4(int n) {
+        if (n % 2 != 0) return -1;
+        int apple = n;
+        while (apple % 8 != 0 && apple > 0) {
+            apple -= 6;
+        }
+        return apple < 0 ? -1 : apple / 8 + (n - apple) / 6;
+    }
+
+    private static int min(int x1, int x2) {
         if (x1 < 0 && x2 < 0) {
             return -1;
         } else if (x1 < 0) {
