1f634bb8a7050bbff2380eca91a6a1897a6173b8
SaigyoujiNono <1029259127@qq.com>
2022-04-14 00:21:18 +0800

    求最大矩形边长

--- question/src/main/java/com/mqd/MaxRectangleLengthOfSide.java
+++ question/src/main/java/com/mqd/MaxRectangleLengthOfSide.java
@@ -11,61 +11,66 @@ package com.mqd;
  */
 public class MaxRectangleLengthOfSide {
     private static final int[][] matrix = {
-            {0, 1, 1, 1, 1},
-            {0, 1, 0, 0, 1},
-            {0, 1, 0, 0, 1},
-            {0, 1, 1, 1, 1},
-            {0, 1, 0, 1, 1}
+            {0, 1, 1, 1, 1, 1},
+            {0, 1, 0, 0, 1, 1},
+            {0, 1, 0, 0, 1, 1},
+            {0, 1, 1, 1, 1, 1},
+            {0, 1, 1, 1, 1, 1}
     };
 
     public static void main(String[] args) {
         System.out.println(solution1(matrix));
     }
 
-    // 解决1 暴力解
+    /**
+     * 解决1 暴力解，遍历所有子矩阵
+     * @param matrix    矩阵
+     * @return  最大边长
+     */
     public static int solution1(int[][] matrix) {
         int res = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == 1) {
-                    int tempRes = 1;
-                    int width = j, height = i;
-                    while (width < matrix.length-1 && height < matrix[0].length-1) {
-                        width++;
-                        height++;
-                        if (matrix[height][width] == 1) {
-                            boolean flag = true;
-                            int w1 = j,w2 = j, h1 = i, h2 = i;
-                            while (w1<=width && w2 <= width && h1 <= height && h2<= height){
-                                if (matrix[i][w1] != 1) {
-                                    flag = false;
-                                    break;
-                                }
-                                if (matrix[height][w2] != 1){
-                                    flag = false;
-                                    break;
-                                }
-                                if (matrix[h1][j] != 1) {
-                                    flag = false;
-                                    break;
-                                }
-                                if (matrix[h2][width] != 1) {
-                                    flag = false;
-                                    break;
-                                }
-                                w1++;
-                                w2++;
-                                h1++;
-                                h2++;
-                            }
-                            if (flag) {
-                                tempRes = width - i;
-                            }
-                            break;
-                        }
-                    }
-                    res = Math.max(res, tempRes);
+        // 遍历所有点
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length ; col++) {
+                // 检查该点最大的矩形边长
+                if (matrix[row][col] == 1) {
+                    res = Math.max(getCurrentMaxLength(matrix,row,col),res);
                 }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 获取当前节点最大矩形长度
+     * @param matrix    大矩形
+     * @param row   当前行
+     * @param col   当前列
+     * @return  当前节点最大矩形长度
+     */
+    public static int getCurrentMaxLength(int[][] matrix, int row, int col) {
+        int row2 = row;
+        int col2 = col;
+        int res = 1;
+        while (row2 + 1 < matrix.length && col2 + 1 < matrix[0].length) {
+            row2++;
+            col2++;
+            if (matrix[row2][col2] == 1) {
+                boolean flag = true;
+                for (int c = col, r = row; c < col2 || r < row2; c++, r++) {
+                    if (matrix[row][c] != 1 || matrix[row2][c] != 1) {
+                        flag = false;
+                        break;
+                    }
+                    if (matrix[r][col] != 1 || matrix[r][col2] != 1) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag){
+                    res = Math.max(res, col2-col+1);
+                }
+
             }
         }
         return res;
