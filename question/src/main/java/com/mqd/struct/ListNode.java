3eb44ee27275d874b592671e7500a0bb8e852edd
SaigyoujiNono <1029259127@qq.com>
2022-05-22 13:12:39 +0800

    leetcode 链表结构：数组构造单链表

--- question/src/main/java/com/mqd/struct/ListNode.java
+++ question/src/main/java/com/mqd/struct/ListNode.java
@@ -1,9 +1,21 @@
 package com.mqd.struct;
 
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
     ListNode() {}
-    ListNode(int val) { this.val = val; }
+    public ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public static ListNode from(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+        ListNode listNode = new ListNode(arr[0]);
+        ListNode cur = listNode;
+        for (int i = 1; i < arr.length; i++) {
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+        return listNode;
+    }
 }
