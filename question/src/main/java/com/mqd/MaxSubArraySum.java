1c4b6f3261050da890f5897cc5ee95b2666274e2
SaigyoujiNono <1029259127@qq.com>
2022-06-19 16:36:43 +0800

    最大连续子数组和(优化空间为O(1))

--- question/src/main/java/com/mqd/MaxSubArraySum.java
+++ question/src/main/java/com/mqd/MaxSubArraySum.java
@@ -7,6 +7,7 @@ public class MaxSubArraySum {
     public static void main(String[] args) {
         int[] arr = new int[]{1,1,-1,-10,11,4,-6,9,20,-10,-2};
         System.out.println(solution(arr));
+        System.out.println(solution2(arr));
     }
 
     public static int solution(int[] arr){
@@ -20,6 +21,22 @@ public class MaxSubArraySum {
                 dp[i] = arr[i] + dp[i - 1];
             }
             res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    public static int solution2(int[] arr){
+        int pre = arr[0];
+        int res = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            int cur;
+            if (pre <= 0) {
+                cur = arr[i];
+            } else {
+                cur = arr[i] + pre;
+            }
+            pre = cur;
+            res = Math.max(res, cur);
         }
         return res;
     }
