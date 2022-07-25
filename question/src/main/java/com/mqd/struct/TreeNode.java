705f1e1730eb79ad10c61bc960818c76255f4619
SaigyoujiNono <1029259127@qq.com>
2022-07-26 01:29:35 +0800

    TreeNode通过数组创建二叉树方法

--- question/src/main/java/com/mqd/struct/TreeNode.java
+++ question/src/main/java/com/mqd/struct/TreeNode.java
@@ -1,5 +1,9 @@
 package com.mqd.struct;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -10,5 +14,46 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public static void main(String[] args) {
+//        Integer[] nums = {0, 1, 2, 3, 4, 3, 4};
+        Integer[] nums = {1, null, 2, 3, 4, null, 5, null, 6};
+        TreeNode byIntArray = createByIntArray(nums);
+        System.out.println(byIntArray.val);
+    }
+
+    public static TreeNode createByIntArray(Integer[] nums) {
+        if (nums == null || nums.length == 0 || nums[0] == null) return null;
+        Deque<Integer> q = new LinkedList<>(Arrays.asList(nums));
+        Deque<TreeNode> nq = new LinkedList<>();
+        Integer integer = q.pollFirst();
+        assert integer != null;
+        TreeNode root = new TreeNode(integer);
+        nq.addLast(root);
+        while (!q.isEmpty()) {
+            Integer head = q.pollFirst();
+            // 左子节点
+            TreeNode node = nq.pollFirst();
+            assert node != null;
+            if (head == null) {
+                node.left = null;
+            } else {
+                node.left = new TreeNode(head);
+            }
+            // 右子节点
+            if (q.isEmpty()) {
+                break;
+            }
+            head = q.pollFirst();
+            if (head == null) {
+                node.right = null;
+            } else {
+                node.right = new TreeNode(head);
+            }
+            if (node.left != null) nq.addLast(node.left);
+            if (node.right != null) nq.addLast(node.right);
+        }
+        return root;
     }
  }
